1922a905c4bf1b79d59df8c5c9fca543dbe4f402
anissa guembre <guembreani@gmail.com>
2021-04-29 03:52:03 -0400

    completed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31;
+let num2 =2;
+let multiply = num1*num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let num3= 40;
+let num4=9;
+let mod = num3 % num4;
+let max = Math.max(2,3,5,7,9,4,20);
+
